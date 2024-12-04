1e7580309ca871460bce7e61997d4217ad03d6be
Abaseen Khan <arbaseen@gmail.com>
2024-12-04 01:34:29 -0500

    added information to the about page

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,45 +1,96 @@
 export default function AboutUs() {
-  return (
-    <div className="bg-gray-100 h-auto py-24">
-      <div className="max-w-5xl mx-auto bg-white p-24 shadow-md rounded-xl">
-        <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">
-          Meet the founders!
-        </h1>
+    return (
+        <div className="bg-gray-100 h-auto py-24">
+            <div className="max-w-5xl mx-auto bg-white p-24 shadow-md rounded-xl">
+                <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">
+                    Our Work
+                </h1>
 
-        <h3 className="text-2xl font-semibold text-gray-700 mb-2">
-          Teena Nguyen (Accountant)
-        </h3>
-        <p className="text-gray-600 mb-8">
-          Teena  has extensive experience in Accounting and tax preparation.
-          Her experience started from working at a public CPA firm for over 8 years on Long Island, NY.
-          She is currently an accountant for a local public school where she thoroughly mastered government accounting/financial reporting.
-          Teena provides pro bono tax preparation to local PTA’s and Saigon Children Charities (USA).
-          She is a co-founder of Vietnam Learning, Arts and Cultural Center (VLACC.org).
-          Teena graduated from Hofstra University with a BBA degree in Finance  and attended SUNY Old Westbury for her accounting credits, which qualifies her for the CPA exam.
-          She is a current member of the National Association of Tax Professionals. She loves gardening, local fairs/farmer’s markets, Zen meditation and spending time with nature.
-        </p>
+                <p className="text-gray-600 mb-8">
+                    The creation of the <strong>Joseph and Susan Pisacano Memorial
+                    Foundation</strong> was inspired by a profound desire to commemorate
+                    the legacy of my late foster parents. The sincere love
+                    demonstrated by Joe and Sue provided invaluable support
+                    during times when I felt disconnected from my own family. In
+                    a modest endeavor, we aim to perpetuate their legacy by
+                    promoting kindness within the community. Each year, the
+                    Foundation awards a scholarship ranging from $500 to $1,000
+                    to a graduating senior from Walt Whitman High School—my
+                    foster parents' hometown—or another local high school. To
+                    promote inclusivity, the eligibility requirements are as
+                    follows: The recipient must: 1. Be a foster child or have
+                    experienced the loss of a parent(s), and 2. Be graduating
+                    from a high school where they are currently enrolled as a
+                    senior. At the Foundation, we are committed to maintaining
+                    adaptable guidelines for the scholarship. Consequently, the
+                    recipient is granted the freedom to utilize the funds in any
+                    manner they deem appropriate—whether for further education,
+                    vocational training, military service, or entering the
+                    workforce.
+                </p>
 
-        <h3 className="text-2xl font-semibold text-gray-700 mb-2">
-          John Lattanzi (Payroll/Bookkeeping)
-        </h3>
-        <p className="text-gray-600 mb-8">
-          John has 7+ years as a wage compliance investigator for the City of New York, and is the Payroll manager for BOSS.
-          He was trained and is well versed in Intuit QuickBooks and Microsoft Excel along with numerous other software applications.
-          His experience as a NYC wage compliance investigator includes knowing the shortfalls of small businesses and their payroll & bookkeeping objectives to ensure that companies stay in compliance.
-        </p>
+                <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">
+                    Meet the founders!
+                </h1>
 
-        <h3 className="text-2xl font-semibold text-gray-700 mb-2">
-          Kim Nguyen (Esq. of Counsel)
-        </h3>
-        <p className="text-gray-600 mb-8">
-          Kim is a licensed New York attorney, who is also admitted to practice in the Eastern and Southern Districts of U.S. Federal Court. 
-          She attended Hunter College for her BA and Pace University for her JD. Kim’s practice ranges from Labor and Employment to brokering business sales, and everything in between. 
-          She has extensive experience negotiating labor contracts. Her focus on Labor and Employment reaches from Federal and State agencies, like the National Labor Relations Board to private arbitration litigation. 
-          Jokingly, Kim likes to say that she can help with everything except for personal injury and (contested) divorces. 
-          Kim truly believes that legal and business knowledge should be available for those who want it; therefore, she has and continues to provide law-rate or pro bono legal counsel to individuals and organizations. 
-          In addition to being an attorney, Kim is a real estate broker, NYS notary, and an ordained minister with the Universal Life Church (thanks to the internet).
-        </p>
-      </div>
-    </div>
-  )
+                <h3 className="text-2xl font-semibold text-gray-700 mb-2">
+                    Teena Nguyen (Accountant)
+                </h3>
+                <p className="text-gray-600 mb-8">
+                    Teena has extensive experience in Accounting and tax
+                    preparation. Her experience started from working at a public
+                    CPA firm for over 8 years on Long Island, NY. She is
+                    currently an accountant for a local public school where she
+                    thoroughly mastered government accounting/financial
+                    reporting. Teena provides pro bono tax preparation to local
+                    PTA’s and Saigon Children Charities (USA). She is a
+                    co-founder of Vietnam Learning, Arts and Cultural Center
+                    (VLACC.org). Teena graduated from Hofstra University with a
+                    BBA degree in Finance and attended SUNY Old Westbury for her
+                    accounting credits, which qualifies her for the CPA exam.
+                    She is a current member of the National Association of Tax
+                    Professionals. She loves gardening, local fairs/farmer’s
+                    markets, Zen meditation and spending time with nature.
+                </p>
+
+                <h3 className="text-2xl font-semibold text-gray-700 mb-2">
+                    John Lattanzi (Payroll/Bookkeeping)
+                </h3>
+                <p className="text-gray-600 mb-8">
+                    John has 7+ years as a wage compliance investigator for the
+                    City of New York, and is the Payroll manager for BOSS. He
+                    was trained and is well versed in Intuit QuickBooks and
+                    Microsoft Excel along with numerous other software
+                    applications. His experience as a NYC wage compliance
+                    investigator includes knowing the shortfalls of small
+                    businesses and their payroll & bookkeeping objectives to
+                    ensure that companies stay in compliance.
+                </p>
+
+                <h3 className="text-2xl font-semibold text-gray-700 mb-2">
+                    Kim Nguyen (Esq. of Counsel)
+                </h3>
+                <p className="text-gray-600 mb-8">
+                    Kim is a licensed New York attorney, who is also admitted to
+                    practice in the Eastern and Southern Districts of U.S.
+                    Federal Court. She attended Hunter College for her BA and
+                    Pace University for her JD. Kim’s practice ranges from Labor
+                    and Employment to brokering business sales, and everything
+                    in between. She has extensive experience negotiating labor
+                    contracts. Her focus on Labor and Employment reaches from
+                    Federal and State agencies, like the National Labor
+                    Relations Board to private arbitration litigation. Jokingly,
+                    Kim likes to say that she can help with everything except
+                    for personal injury and (contested) divorces. Kim truly
+                    believes that legal and business knowledge should be
+                    available for those who want it; therefore, she has and
+                    continues to provide law-rate or pro bono legal counsel to
+                    individuals and organizations. In addition to being an
+                    attorney, Kim is a real estate broker, NYS notary, and an
+                    ordained minister with the Universal Life Church (thanks to
+                    the internet).
+                </p>
+            </div>
+        </div>
+    );
 }
